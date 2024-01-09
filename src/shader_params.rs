949af1421f73f7d6c55de46c333b4c1c53717b99
Mark Wainwright <wainwrightml@gmail.com>
2024-01-09 14:22:43 +0000

    added std::ops for linearrgba and linearrgb

--- src/shader_params.rs
+++ src/shader_params.rs
@@ -20,6 +20,32 @@ pub struct LinearRGBA {
     pub alpha: f32,
 }
 
+impl std::ops::Mul<f32> for LinearRGBA {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+            alpha: self.alpha * rhs,
+        }
+    }
+}
+
+impl std::ops::Add for LinearRGBA {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            red: self.red + rhs.red,
+            green: self.green + rhs.green,
+            blue: self.blue + rhs.blue,
+            alpha: self.alpha + rhs.alpha,
+        }
+    }
+}
+
 impl From<bevy::prelude::Color> for LinearRGBA {
     fn from(value: bevy::prelude::Color) -> Self {
         let [red, green, blue, alpha] = value.as_linear_rgba_f32();
@@ -44,5 +70,29 @@ impl From<bevy::prelude::Color> for LinearRGB {
     fn from(value: bevy::prelude::Color) -> Self {
         let [red, green, blue, _alpha] = value.as_linear_rgba_f32();
         Self { red, green, blue }
+    }
+}
+
+impl std::ops::Mul<f32> for LinearRGB {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+        }
+    }
+}
+
+impl std::ops::Add for LinearRGB {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            red: self.red + rhs.red,
+            green: self.green + rhs.green,
+            blue: self.blue + rhs.blue,
+        }
     }
 }
